49257e42490fbeac61483563c7a7a736245a46f8
Hirohito Kato <support@cielo.rojo.jp>
2014-04-06 11:15:08 +0900

    Initial Commit

---
+++ HKLSocketStubServer/HKLSocketStubServer.h
@@ -0,0 +1,13 @@
+//
+//  HKLSocketStubServer.h
+//  HKLSocketStubServer
+//
+//  Created by Hirohito Kato on 2014/04/06.
+//  Copyright (c) 2014年 KatokichiSoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HKLSocketStubServer : NSObject
+
+@end
---
+++ HKLSocketStubServer/HKLSocketStubServer.m
@@ -0,0 +1,13 @@
+//
+//  HKLSocketStubServer.m
+//  HKLSocketStubServer
+//
+//  Created by Hirohito Kato on 2014/04/06.
+//  Copyright (c) 2014年 KatokichiSoft. All rights reserved.
+//
+
+#import "HKLSocketStubServer.h"
+
+@implementation HKLSocketStubServer
+
+@end
